a994af78aa01607d8f0db77344375b723c6fd06f
tomas <tomgcc2@gmail.com>
2026-06-18 15:33:12 -0500

    Create conections.js

---
+++ conections.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAwm2MIN-R_QRlKQpe2emwB3nELPGe0fEg",
+  authDomain: "pricenice-8afb7.firebaseapp.com",
+  projectId: "pricenice-8afb7",
+  storageBucket: "pricenice-8afb7.firebasestorage.app",
+  messagingSenderId: "500151573638",
+  appId: "1:500151573638:web:2d336d461ec65188c98507"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
